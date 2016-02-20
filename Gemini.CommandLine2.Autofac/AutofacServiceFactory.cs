8b184b01b1866fe6b924d2c636cb3b358f1cc19b
Dave Van den Eynde <dave@groovbird.com>
2016-02-20 21:00:46 +0100

    Adding Autofac support.

---
+++ Gemini.CommandLine2.Autofac/AutofacServiceFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autofac;
+
+namespace Gemini.CommandLine2.Autofac
+{
+    public class AutofacServiceFactory : IServiceFactory
+    {
+        private readonly IContainer _container;
+
+        public AutofacServiceFactory(IContainer container)
+        {
+            _container = container;
+        }
+
+        public object Create(Type type)
+        {
+            return _container.Resolve(type);
+        }
+    }
+}
---
+++ Gemini.CommandLine2.Autofac/CommandExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace Gemini.CommandLine2.Autofac
+{
+    public static class CommandExtensions
+    {
+        public static Command UseAutofac(this Command command, IContainer container)
+        {
+            command.ServiceFactory = new AutofacServiceFactory(container);
+
+            return command;
+        }
+    }
+}
