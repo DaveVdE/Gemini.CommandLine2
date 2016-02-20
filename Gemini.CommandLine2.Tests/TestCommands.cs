aa5fd83dff336518924b42e754808e8cc16ef29f
Dave Van den Eynde <dave@groovbird.com>
2016-02-20 21:01:03 +0100

    Adding some test code.

--- Gemini.CommandLine2.Tests/TestCommands.cs
+++ Gemini.CommandLine2.Tests/TestCommands.cs
@@ -1,9 +1,15 @@
 ﻿namespace Gemini.CommandLine2.Tests
 {
+    /// <summary>
+    /// Contains commands to use with testing.
+    /// </summary>
     public class TestCommands
     {
         public static bool CommandWithNoArgumentsRan { get; set; }
-
+       
+        /// <summary>
+        /// Performs a test of a command that requires no arguments.
+        /// </summary>
         public void CommandWithNoArguments()
         {
             CommandWithNoArgumentsRan = true;
